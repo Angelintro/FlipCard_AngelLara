ddabc63f16b85871810ecf3d0e695aea70b2e38f
Angelintro <43738504+Angelintro@users.noreply.github.com>
2019-02-13 17:57:28 -0700

    Initial commit

---
+++ app/src/main/java/com/example/angel/asdasd/MainActivity.java
@@ -0,0 +1,119 @@
+package com.example.angel.asdasd;
+
+import android.app.Activity;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import static android.R.id.button1;
+import static android.R.id.button2;
+
+public class MainActivity extends Activity {
+    /**
+     * A handler object, used for deferring UI operations.
+     */
+    private Handler mHandler = new Handler();
+
+    /**
+     * Whether or not we're showing the back of the card (otherwise showing the front).
+     */
+    private boolean mShowingBack = false;
+
+
+    private Button button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_flip);
+
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                flipCard();
+            }
+        });
+
+        if (savedInstanceState == null) {
+            // If there is no saved instance state, add a fragment representing the
+            // front of the card to this activity. If there is saved instance state,
+            // this fragment will have already been added to the activity.
+            getFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.container, new CardFrontFragment())
+                    .commit();
+        }
+
+
+    }
+
+    private void flipCard() {
+        if (mShowingBack) {
+            getFragmentManager().popBackStack();
+            return;
+        }
+
+        // Animacion de voltear atras
+
+        mShowingBack = true;
+
+        // Create and commit a new fragment transaction that adds the fragment for the back of
+        // the card, uses custom animations, and is part of the fragment manager's back stack.
+
+        getFragmentManager()
+                .beginTransaction()
+
+                // Replace the default fragment animations with animator resources representing
+                // rotations when switching to the back of the card, as well as animator
+                // resources representing rotations when flipping back to the front (e.g. when
+                // the system Back button is pressed).
+                .setCustomAnimations(
+                        R.animator.card_flip_right_in, R.animator.card_flip_right_out,
+                        R.animator.card_flip_left_in, R.animator.card_flip_left_out)
+
+                // Replace any fragments currently in the container view with a fragment
+                // representing the next page (indicated by the just-incremented currentPage
+                // variable).
+                .replace(R.id.container, new CardBackFragment())
+
+                // Add this transaction to the back stack, allowing users to press Back
+                // to get to the front of the card.
+                .addToBackStack(null)
+
+                // Commit the transaction.
+                .commit();
+    }
+
+
+    /**
+     * Fragmento de la imacen frontal
+     */
+    public static class CardFrontFragment extends android.app.Fragment {
+        public CardFrontFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_card_front, container, false);
+        }
+    }
+
+    /**
+     * Fragmento de la imacen tracera
+     */
+    public static class CardBackFragment extends android.app.Fragment {
+        public CardBackFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_card_back, container, false);
+        }
+    }
+}
